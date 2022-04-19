6d94da6bc2426a6696de2c52274d3a3dde1f95cb
Manny <mannysth@yahoo.com>
2022-04-19 15:45:10 -0700

    passes all the tests and pushes to github

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import TopSpots from './topspots';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      topspots: []
+    };
+  }
+
+  componentWillMount() {
+    axios.get('https://origin-top-spots-api.herokuapp.com/api/topspots')
+      .then(res => res.data)
+      .then(topspots => this.setState({ topspots }));
+  }
+
   render() {
     return (
-      <div className='App'></div>
+      <div className='container'>
+        <div className='jumbotron'>
+          <h1>San Diego Top Spots</h1>
+          <p>A list of the top 30 places to see in San Diego, California</p>
+        </div>
+        {
+          this.state.topspots.map(topspot => (
+            <TopSpots
+              key={ topspot.id }
+              name={ topspot.name }
+              description={ topspot.description }
+              location={ topspot.location }
+            />
+          ))
+        }
+      </div>
     );
   }
 }
---
+++ src/topspots.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const TopSpots = (props) => {
+  const url = `https://maps.google.com/?q=${props.location[0]},${props.location[1]}`
+  return (
+    <div className='well'>
+      <h4>{props.name}</h4>
+      <p>{props.description}</p>
+      <a href={ url }>Google Link</a>
+    </div>
+  );
+};
+
+TopSpots.propTypes = {
+  name: React.PropTypes.string,
+  description: React.PropTypes.string,
+  location: React.PropTypes.arrayOf
+};
+
+export default TopSpots;
