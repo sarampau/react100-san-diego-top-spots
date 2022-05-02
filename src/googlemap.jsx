9d62359f951f4e7e61b275b05006e0b87752a055
Manny <mannysth@yahoo.com>
2022-05-02 16:08:45 -0700

    finishes project and pushes to github

--- src/googlemap.jsx
+++ src/googlemap.jsx
@@ -19,20 +19,37 @@ class GoogleMap extends Component {
         zoom={ 9 }
         style={ mapStyles }
         className='google-map'
-        initialCenter={ { lat: 32.81, lng: -117 } }
+        initialCenter={ { lat: 32.85, lng: -117.15 } }
+        scrollwheel={ false }
+        draggable={ false }
+        disableDoubleCLickZoom={ false }
+        zoomControl={ false }
+        mapTypeControl={ false }
+        scaleControl={ false }
+        streetViewControl={ false }
+        panControl={ false }
+        rotateControl={ false }
+        fullScreenControl={ false }
       >
         {
           this.props.topspots.map((topspot, i) => {
-            return <Marker
-              key={i}
-              position={{lat: topspot.location[0], lng: topspot.location[1]}}
-            />
+            return (
+              <Marker
+                key={ i }
+                position={ { lat: topspot.location[0], lng: topspot.location[1] } }
+              />
+            );
           })
         }
       </Map>
     );
   }
 }
+
+GoogleMap.propTypes = {
+  google: React.PropTypes.objectOf,
+  topspots: React.PropTypes.arrayOf
+};
 
 export default GoogleApiWrapper({
   apiKey: API_KEY
--- src/topspots.jsx
+++ src/topspots.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 const TopSpots = (props) => {
   const url = `https://maps.google.com/?q=${props.location[0]},${props.location[1]}`;
   return (
-    <div className='well'>
-      <h2>{props.name}</h2>
-      <p>{props.description}</p>
-      <a href={ url }>Google Link</a>
-    </div>
+    <a href={ url } target='_blank' rel='noreferrer noopener'>
+      <div className='well'>
+        <h2>{props.name}</h2>
+        <p>{props.description}</p>
+      </div>
+    </a>
   );
 };
 
